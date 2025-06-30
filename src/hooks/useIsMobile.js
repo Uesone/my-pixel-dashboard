a77eae354063ab3d24c967446e785ec95c0f045e
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2025-06-30 23:58:45 +0200

    yo
    yo

--- src/hooks/useIsMobile.js
+++ src/hooks/useIsMobile.js
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 
 export default function useIsMobile() {
   const [isMobile, setIsMobile] = useState(
-    typeof window !== "undefined" ? window.innerWidth < 600 : false
+    typeof window !== "undefined" ? window.innerWidth < 900 : false
   );
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 600);
+      setIsMobile(window.innerWidth < 900);
     };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
